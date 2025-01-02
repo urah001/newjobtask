e52820a5334ce48267421fb6263f1827d77366c5
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-01-02 16:30:25 +0100

    fetching data

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+// this is the home directory
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 
-function Home(){
+function Home() {
+  const [getUsers, setgetUsers] = useState();
+  useEffect(() => {
+    const fecthUsers = async () => {
+      const resq = await fetch("https://jsonplaceholder.typicode.com/users");
+      const resp = await resq.json();
+      setgetUsers(resp);
+    };
+    fecthUsers();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">Home</header>
       <section className="App-section">
-        many content would be here 
+        <h1>user list</h1>
+        <ul>
+          {getUsers.map((user) => (
+            <li>
+              <Link to={`/user/${user.id}`}> view detail</Link>
+            </li>
+          ))}
+        </ul>
       </section>
-      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

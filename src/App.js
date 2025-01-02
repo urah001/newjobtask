88bf91e14a0a452e9c66715c5e120676b7477505
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-01-02 18:43:27 +0100

    imported component for router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {Home , DetailPage} from "./components/home";
+
+import Home from "./components/home";
+import DetailPage from "./components/detail";
 
 function App() {
   return (
